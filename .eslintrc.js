65cc794b00ce08dfba2090a664480fc0e3af05a5
Jojo E. Tutor <jeshrave003@gmail.com>
2019-06-08 16:15:13 +0800

    setup amplify

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+    "extends": "airbnb",
+    "rules": {
+        "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
+        "react/jsx-one-expression-per-line": 0,
+        "linebreak-style": 0,
+    },
+};
--- src/App.js
+++ src/App.js
@@ -1,26 +1,85 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import Amplify, { Auth, Analytics, Storage } from 'aws-amplify';
+import {
+  withAuthenticator, S3Album, S3Image, PhotoPicker,
+} from 'aws-amplify-react';
 import './App.css';
+import awsconfig from './aws-exports';
 
-function App() {
+Amplify.configure(awsconfig);
+
+const Photos = ({ list, theme }) => (
+  <div className="album" style={theme.album}>
+    {list.map(({ key }) => <S3Image theme={theme} style={theme.photoStyle} key={key} imgKey={key} />)}
+  </div>
+);
+
+function App(props) {
+  const [file, setFile] = useState(null);
+  const [list, setList] = useState([]);
+
+  const getFiles = () => {
+    Storage.list('')
+      .then((result) => {
+        console.log('@result', result);
+        setList(result);
+      })
+      .catch(err => console.log(err));
+  };
+
+  useEffect(() => {
+    Analytics.record('Amplify_CLI');
+    getFiles();
+  }, []);
+
+  const logout = () => {
+    Auth.signOut()
+      .then(data => console.log(data))
+      .catch(err => console.log(err));
+  };
+
+  const uploadFile = ({ name, file }) => {
+    Storage
+      .put(name, file)
+      .then(() => {
+        setFile(name);
+      });
+  };
+
+  const theme = {
+    album: {
+      display: 'flex',
+      flexWrap: 'wrap',
+      justifyContent: 'center',
+      width: '600px',
+    },
+    photoStyle: {
+      width: '200px',
+      height: '200px',
+    },
+    photoImg: {
+      width: '100%',
+      height: 'auto',
+    },
+    pickerPreview: {
+      width: '400px',
+    },
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="App">
+          <p> Pick a file</p>
+          <PhotoPicker preview onPick={uploadFile} theme={theme} />
+          <Photos key={file} list={list} theme={theme} />
+        </div>
+        <button type="button" onClick={() => logout()}>
+          Sign Out
+        </button>
       </header>
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
